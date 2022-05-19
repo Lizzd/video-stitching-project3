f99294baad8b208ecacb1c8dccea4db545b8fddf
Lizzd <1422139436@qq.com>
2022-05-19 14:33:55 +0200

    “background_detect”

--- background-sub.py
+++ background-sub.py
@@ -5,16 +5,18 @@ import numpy as np
 
 parser1 = argparse.ArgumentParser(description='This program shows how to use background subtraction methods provided by \
                                               OpenCV. You can process both videos and images.')
-parser1.add_argument('--input', type=str, help='Path to a video or a sequence of image.', default='project1.avi')
+parser1.add_argument('--input', type=str, help='Path to a video or a sequence of image.', default='projectmoving.avi')
 parser1.add_argument('--algo', type=str, help='Background subtraction method (KNN, MOG2).', default='MOG2')
 args1 = parser1.parse_args()
 
 parser2 = argparse.ArgumentParser(description='This program shows how to use background subtraction methods provided by \
                                               OpenCV. You can process both videos and images.')
-parser2.add_argument('--input', type=str, help='Path to a video or a sequence of image.', default='mask1.avi')
+parser2.add_argument('--input', type=str, help='Path to a video or a sequence of image.', default='maskmoving.avi')
 parser2.add_argument('--algo', type=str, help='Background subtraction method (KNN, MOG2).', default='MOG2')
 args2 = parser2.parse_args()
 
+mask_border = cv.imread('border_mask.jpg');
+mask_border_resize = cv.resize(mask_border[:, :, 0], [754, 352])
 if args1.algo == 'MOG2':
     backSub1 = cv.createBackgroundSubtractorMOG2()
     backSub2 = cv.createBackgroundSubtractorMOG2()
@@ -31,18 +33,30 @@ while True:
     ret, mask = capture2.read()
     if frame is None:
         break
-    frame = cv.blur(frame, (30, 30))
+    # frame = cv.blur(frame, (10, 10))
 
     # element = cv.getStructuringElement(cv.MORPH_CROSS, (5, 5))
     # frame = cv.morphologyEx(frame, cv.MORPH_OPEN, element)
 
     fgMask = backSub1.apply(frame)
     # fgMask = fgMask * 0
-    fgMask = fgMask * mask[:, :, 0] / 255.0
-    # fgMask = cv.blur(fgMask, (10, 10))
-    # element = cv.getStructuringElement(cv.MORPH_CROSS, (5, 5))
-    # fgMask = cv.morphologyEx(fgMask, cv.MORPH_OPEN, element)
 
+    fgMask = fgMask * (mask[:, :, 0] / 255) * (mask_border_resize / 255)
+
+    # fgMask = fgMask * (mask[:, :, 0]/255)
+    # fgMask = cv.blur(fgMask, (3, 3))
+
+    element = cv.getStructuringElement(cv.MORPH_CROSS, (5, 5))
+    fgMask = cv.morphologyEx(fgMask, cv.MORPH_OPEN, element)
+    moving_obj = np.zeros(frame.shape)
+    moving_obj[:, :, 0] = fgMask/255 * frame[:, :, 0]/255
+    moving_obj[:, :, 1] = fgMask/255 * frame[:, :, 1]/255
+    moving_obj[:, :, 2] = fgMask/255 * frame[:, :, 2]/255
+
+    background = np.zeros(frame.shape)
+    background[:, :, 0] = (255 - fgMask)/255 * frame[:, :, 0]/255
+    background[:, :, 1] = (255 - fgMask)/255 * frame[:, :, 1]/255
+    background[:, :, 2] = (255 - fgMask)/255 * frame[:, :, 2]/255
     #
     cv.rectangle(frame, (10, 2), (100, 20), (255, 255, 255), -1)
     cv.putText(frame, str(capture1.get(cv.CAP_PROP_POS_FRAMES)), (15, 15),
@@ -50,7 +64,9 @@ while True:
 
     cv.imshow('Frame', frame)
     cv.imshow('FG Mask', fgMask)
-    # cv.imshow('mask',mask)
+    cv.imshow('mask', mask[:, :, 0])
+    cv.imshow('moving_obj', moving_obj)
+    cv.imshow('background', background)
     # height, width = fgMask.shape
     # size = (width, height)
     # fgMask_Video = np.array([[fgMask], [fgMask], [fgMask]])
@@ -58,7 +74,7 @@ while True:
     # fgMask_Video = fgMask_Video.reshape([height,width, 3])
     #
     # mask_array.append(fgMask_Video)
-    keyboard = cv.waitKey(30)
+    keyboard = cv.waitKey(500)
     if keyboard == 'q' or keyboard == 27:
         break
 # out = cv.VideoWriter('mask3.avi', cv.VideoWriter_fourcc(*'DIVX'), 15, size)
--- creat_video.py
+++ creat_video.py
@@ -3,13 +3,17 @@ import numpy as np
 import glob
 
 img_array = []
-for filename in glob.glob('./image_stiched/mask*.jpg'):
+# for filename in glob.glob('./frames/fixed_frame*.jpg'):
+for i in range(43):
+    # filename = './frames/fixed_frame' + str(i) + '.jpg'
+    filename = './mask/mask_fixed_frame' + str(i) + '.jpg'
     img = cv2.imread(filename)
     height, width, layers = img.shape
     size = (width, height)
     img_array.append(img)
 
-out = cv2.VideoWriter('mask1.avi', cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
+out = cv2.VideoWriter('maskmoving.avi', cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
+# out = cv2.VideoWriter('projectmoving.avi', cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
 
 for i in range(len(img_array)):
     out.write(img_array[i])
